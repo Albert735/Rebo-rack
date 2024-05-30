350f6c701cadd66374d2cc32c97c87fcf8fd9a2e
Albert735 <03jayteee@gmail.com>
2024-05-30 12:38:24 +0000

    inter and besport font commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,21 +3,29 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center text-white w-[100dvw] ">
-      <div className="p-7">
-        <span>Rebo</span>
+      <div className="p-7 ">
+        <span className="besport">Rebo</span>
         <span>Rack</span>
       </div>
-      <div className="p-7">
-        <ul className="flex justify-center items-center gap-10">
-          <li>Home</li>
-          <li>License</li>
-          <li>About</li>
-          <li>Create Music</li>
+      <div className=" bg-white/10 rounded-full px-5">
+        <ul className="flex justify-center items-center gap-[4dvw] p-2.5">
+          <li className="hover:bg-white hover:text-black transition-all ease-in-out px-2 py-1 rounded-full">
+            Home
+          </li>
+          <li className="hover:bg-white hover:text-black transition-all ease-in-out px-2 py-1 rounded-full">
+            About
+          </li>
+          <li className="hover:bg-white hover:text-black transition-all ease-in-out px-2 py-1 rounded-full">
+            License
+          </li>
+          <li className="hover:bg-white hover:text-black transition-all ease-in-out px-2 py-1 rounded-full">
+            Create Music
+          </li>
         </ul>
       </div>
-      <div className="p-7">
-        <button>Log in</button>
-        <button>Sign Up</button>
+      <div className="flex p-7 gap-[1dvw]">
+        <button className="border-2 rounded-full px-2 py-1">Log in</button>
+        <button className="border-2 border-[#ed8f01] bg-[#ed8f01] rounded-full px-2 py-1">Sign Up</button>
       </div>
     </div>
   );
