c86fc25f47818509a393123b3b669e620c6f9aa2
Albert735 <03jayteee@gmail.com>
2024-05-30 14:20:18 +0000

    hero ui complete

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,27 +1,43 @@
 import React from "react";
 import man1 from "../assets/man1.jpg";
-import man2 from "../assets/man2.jpg"
+import man2 from "../assets/man2.jpg";
 
 const Hero = () => {
   return (
-    <div className="flex relative justify-center items-center text-white">
+    <div className="flex relative justify-center items-center text-white gap-2">
       <div>
         <span className="exordar text-[25rem]">R</span>
       </div>
-      <div className="flex -space-x-10">
-        <div className="bg-[#ed8f01] h-[28rem] w-[28rem] rounded-full z-10"></div>{" "}
+      <div className="flex -space-x-20">
+        <div className="bg-[#ed8f01] h-[25rem] w-[25rem] rounded-full z-10  relative">
+          <div className="flex absolute flex-col justify-center items-start top-[8rem] left-[3rem]">
+            <p className="leading-loose">
+              Create your own beats with the <br />
+              power of AI
+            </p>
+            <p className="font-bold text-[1.25rem]">
+              Welcome to thw music <br /> AI generation
+            </p>
+            <button className="text-black bg-white px-2 py-1 rounded-full my-3">
+              Try it for free
+            </button>
+          </div>
+        </div>
         <img
           src={man1}
           alt=""
-          className="rounded-full h-[28rem] w-[28rem] object-cover z-20"
+          className="rounded-full h-[25rem] w-[25rem] object-cover z-20"
         />
+        
         <img
           src={man2}
           alt=""
-          className="rounded-full h-[28rem] w-[28rem] object-cover z-30"
+          className="rounded-full h-[25rem] w-[25rem] object-cover z-30"
         />
       </div>
-      <div></div>
+      <div>
+        <span className="exordar text-[25rem] ">O</span>
+      </div>
     </div>
   );
 };
